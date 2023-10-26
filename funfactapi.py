7b5de31bec4dfe682e70c9c1080829dc4fc53f75
Junemetzler <145152065+Junemetzler@users.noreply.github.com>
2023-10-26 14:44:32 -0400

    Update funfactapi.py

--- funfactapi.py
+++ funfactapi.py
@@ -1,9 +1,26 @@
 import requests
 
-limit = 3
-api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
-response = requests.get(api_url, headers={'X-Api-Key': '526QR0kJwoLViPbK+krDiw==mTa6VQSbMiq47NWo'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
+# Define the API endpoint URL
+api_url = "https://api.api-ninjas.com/v1/facts"
+
+# Replace 'YOUR_API_KEY' with your actual API key
+api_key = "526QR0kJwoLViPbK+krDiw==mTa6VQSbMiq47NWo"
+
+# Set up the headers with the API key
+headers = {
+    "APIKey": api_key
+}
+
+# Make the GET request to the API
+response = requests.get(api_url, headers=headers)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    # Extract and print the fact
+    fact = data.get("fact")
+    print("Random Fact:", fact)
 else:
-    print("Error:", response.status_code, response.text)
+    # If the request was not successful, print an error message
+    print("Error:", response.status_code)
